f56d81452e4b8590a2c56c511462f6d10a6e0a89
Shahzaib Khan <shahzaibkhanisme@gmail.com>
2023-04-21 00:50:59 -0700

    create early base in html and js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # etch-a-sketch
 odin JS project
+going to be creating a browser version of sketchpad/etch-a-sketch 
+
---
+++ script.js
@@ -0,0 +1,8 @@
+const flexContainer = document.querySelector("#flex1");
+const flexContainer2 = document.querySelector("#flex2");
+const flexContainer3 = document.querySelector("#flex3");
+const flexContainer4 = document.querySelector("#flex4");
+
+const box = document.createElement('div');
+box.textContent = "maow";
+flexContainer.appendChild(box);
