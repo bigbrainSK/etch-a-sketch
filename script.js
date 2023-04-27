2e422f4b1e781e573ddee476ee6ee2c6f1edf343
Shahzaib Khan <shahzaibkhanisme@gmail.com>
2023-04-27 00:43:08 -0700

    create working loop that creates divs and fitting css

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-const flex1 = document.querySelector(".flex1");
-const flex2 = document.querySelector(".flex2");
-const flex3 = document.querySelector(".flex3");
-const flex4 = document.querySelector(".flex4");
-const mainContainer = document.querySelector("#container1");
-
-//first container
-const box1 = document.createElement('div');
-flex1.appendChild(box1);
+//can I create a loop for declaring each of the 16 containers (?)
+const container = document.querySelector("#container");
+let box1 = document.createElement('div');
+container.appendChild(box1);
 box1.addEventListener("mouseover", colorChange); 
-
-
-
-
-//second container
-const baux1 = document.createElement('div');
-flex2.appendChild(baux1);
-baux1.addEventListener("mouseover", colorChange);
-
-
-
-//third container
-const bahx1 = document.createElement('div');
-flex3.appendChild(bahx1);
-bahx1.addEventListener("mouseover", colorChange);
-
-
-
-//fourth container
-const bahks1 = document.createElement('div');
-flex4.appendChild(bahks1);
-bahks1.addEventListener("mouseover", colorChange);
 
 
 let divAdd = document.createDocumentFragment() 
 
-for (let i=0; i<16; i++) {
-    let newDiv = document.createElement('div');
-    newDiv.class = 'flex' + i;
-    divAdd.appendChild(newDiv);
-    newDiv.addEventListener("mouseover", colorChange); 
-    }
-
+for (let i=0; i<256; i++) {
+    box1 = document.createElement('div');
+    container.appendChild(box1);
+    box1.addEventListener("mouseover", colorChange); 
+    console.log('meow');
+}
 
 
 function colorChange() {
