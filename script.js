7a2695a4d8a6492692421949ed74482292ec93a9
Shahzaib Khan <shahzaibkhanisme@gmail.com>
2023-05-01 21:49:34 -0700

    clean up css to make grid even at the bottom

--- script.js
+++ script.js
@@ -15,24 +15,50 @@ for (let i=0; i<grid; i++) {
     box.addEventListener("click", revertColor);     //function to revert the color with a click
 }
 
-function gridButton () {
-    for (let i=0; i<100; i++) {
-        if (userPrompt==i+'x'+i) {
-            grid === i*i;
-        }
-        else {
-            grid === 16*16;
-        }
-        console.log('woof');
-    }
+// function gridButton () {
+//     for (let i=0; i<100; i++) {
+//         if (userPrompt==i+'x'+i) {
+//             grid === i*i;
+//         }
+//         else {
+//             grid === 16*16;
+//         }
+//         console.log('woof');
+//     }
     // if (prompt=='4x4') {grid === 16};
+//}
+
+
+function gridButton () {
+    switch(userPrompt) {
+    case '2x2': grid = 4; 
+        break;
+    case '3x3': grid = 9;
+        break;
+    case '4x4': grid = 16;
+        break;
+    case '5x5': grid = 25;
+        break;
+    case '6x6': grid = 36;
+        break;
+    case '7x7': grid = 49;
+        break;
+    case '8x8': grid = 64;
+        break;
+    case '9x9': grid = 81;
+        break;
+    case '10x10':grid = 100;
+        break;
+    default: 
+        grid = 256;
+    }  
 }
+    
     
 function gridPrompt() {   
      let userPrompt = prompt("please select desired grid dimensions, in the format of'3x3'." );
      alert(userPrompt);
      console.log('worked');
-     return gridButton();
 }
 
 function colorChange() {
